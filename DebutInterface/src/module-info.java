6bd596a7a1935e51a12072b89389b8f4f2f9a3f7
Occalepsus <juju.joly@free.fr>
2022-03-20 19:39:55 +0100

    Je fais le ménage

--- DebutInterface/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module pro {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	requires transitive javafx.graphics;
-	requires transitive javafx.base;
-	opens application to javafx.fxml;
-	exports application to javafx.graphics;
-}
