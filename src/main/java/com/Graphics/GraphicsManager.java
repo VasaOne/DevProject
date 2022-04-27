df4d2243accdb5fd16fee023a2f3a5b84d457d4d
Occalepsus <juju.joly@free.fr>
2022-04-27 17:44:12 +0200

    Petites mod

--- src/main/java/com/Graphics/GraphicsManager.java
+++ src/main/java/com/Graphics/GraphicsManager.java
@@ -49,24 +49,24 @@ public class GraphicsManager extends Application {
         currentSheet.ioComponent.addEndNode(currentSheet);
 
         // We create 3 new components, which are the most basic ones we'll be using
-        SheetObject orDoor = new SheetObject("or", Color.BLUE, 2, 1, new Boolean[]{false, true, true, true});
+        SheetObject orDoor = new SheetObject("or", Color.PINK, 2, 1, new Boolean[]{false, true, true, true});
         SheetObject notDoor = new SheetObject("not", Color.web("#772288"), 1, 1, new Boolean[] {true,false});
         SheetObject andDoor = new SheetObject("and", Color.web("#03C93C"), 2, 1, new Boolean[]{false, false, false, true});
 
-        SheetObject test = new SheetObject("test", Color.PURPLE, 7, 8, new Boolean[]{true});
+        //SheetObject test = new SheetObject("test", Color.PURPLE, 7, 8, new Boolean[]{true});
 
         // We create instances of these components
         ComponentInstance or = new ComponentInstance(orDoor, 12, 6);
-        //ComponentInstance not = new ComponentInstance(notDoor, 18, 8);
+        ComponentInstance not = new ComponentInstance(notDoor, 18, 8);
         ComponentInstance and = new ComponentInstance(andDoor, 5, 3);
-        ComponentInstance testInstance = new ComponentInstance(test, 18, 8);
+        //ComponentInstance testInstance = new ComponentInstance(test, 18, 8);
 
         // And we place them on the sheet
         currentSheet.addObject(or);
-        //currentSheet.addObject(not);
+        currentSheet.addObject(not);
         currentSheet.addObject(and);
 
-        currentSheet.addObject(testInstance);
+        //currentSheet.addObject(testInstance);
 
         // We create a new wire between the first input and the add door
         // Creates a new wire
